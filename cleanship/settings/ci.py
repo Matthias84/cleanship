1faf2f3b7e44aa5591930ec9df138b4c9f3a2a51
matthias <mame@plattform32.de>
2019-11-20 21:29:40 +0100

    build: Add travis settings

---
+++ cleanship/settings/ci.py
@@ -0,0 +1,25 @@
+from .base import *
+
+# Setup for Travis CI and test coverage via coveralls
+
+ADMIN_ENABLED = False
+
+DEBUG = True
+
+SECRET_KEY = 'klOPsd8sd9a0svvßcy9w8äsoa028='
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'cleanship',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+}
+
+LANGUAGE_CODE = 'de-de'
+
+TIME_ZONE = 'Europe/Berlin'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
