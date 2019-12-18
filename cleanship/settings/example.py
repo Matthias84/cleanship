0bd66345b0f280db7be9dd66234dcd64e001866b
matthias <mame@plattform32.de>
2019-12-18 21:18:43 +0100

    fix: Bad zoom at admin new issues closes #67

--- cleanship/settings/example.py
+++ cleanship/settings/example.py
@@ -27,6 +27,7 @@ TIME_ZONE = 'Europe/Berlin'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LEAFLET_CONFIG = {
+    'SPATIAL_EXTENT': (11.681900024414062, 54.037618883628134, 12.448196411132812, 54.26161470169560),
     'TILES': [
                 ('ORKa.MV', 'http://www.orka-mv.de/geodienste/orkamv/tiles/1.0.0/orkamv/GLOBAL_WEBMERCATOR/{z}/{x}/{y}.png', {'attribution': '&copy; Kartenbild Hanse- und Universitätsstadt Rostock (CC BY 4.0) | Kartendaten OpenStreetMap (ODbL) und LkKfS-MV.'}),
                 ('OpenStreetMap (DE)', 'http://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png', {'attribution': '&copy; OpenStreetMap - Mitwirkende'})
