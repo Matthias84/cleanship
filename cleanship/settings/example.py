06cfa520d8fbbd4fa94004e496f9790790c6a803
matthias <mame@plattform32.de>
2019-12-19 08:39:05 +0100

    feat: Add DOP layer close #8 with template from https://github.com/rostock/datenerfassung/blob/master/datenmanagement/templates/daddddtenmanagement/datamap.html

--- cleanship/settings/example.py
+++ cleanship/settings/example.py
@@ -30,7 +30,8 @@ LEAFLET_CONFIG = {
     'SPATIAL_EXTENT': (11.681900024414062, 54.037618883628134, 12.448196411132812, 54.26161470169560),
     'TILES': [
                 ('ORKa.MV', 'http://www.orka-mv.de/geodienste/orkamv/tiles/1.0.0/orkamv/GLOBAL_WEBMERCATOR/{z}/{x}/{y}.png', {'attribution': '&copy; Kartenbild Hanse- und Universitätsstadt Rostock (CC BY 4.0) | Kartendaten OpenStreetMap (ODbL) und LkKfS-MV.'}),
-                ('OpenStreetMap (DE)', 'http://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png', {'attribution': '&copy; OpenStreetMap - Mitwirkende'})
+                ('OpenStreetMap (DE)', 'http://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png', {'attribution': '&copy; OpenStreetMap - Mitwirkende'}),
+                ('Luftbild 2016', 'https://geo.sv.rostock.de/geodienste//luftbild_mv-40/tiles/1.0.0/hro.luftbild_mv-40.luftbild_mv-40/GLOBAL_WEBMERCATOR/{z}/{x}/{y}.png', {'attribution': '&copy; GeoBasis-DE/M-V'}),
     ]
 }
 
