f5e40b8fa5545e1a760fda8559039cb619c2c167
matthias <mame@plattform32.de>
2020-01-04 10:45:17 +0100

    Add issue detail page

--- office/views.py
+++ office/views.py
@@ -4,7 +4,7 @@ from django.views import generic
 from django_tables2.views import SingleTableMixin
 from django_filters.views import FilterView
 
-from common.models import Issue, IssueFilter
+from common.models import Issue, IssueFilter, StatusTypes, TrustTypes
 from .tables import IssueTable
 
 @login_required
@@ -18,6 +18,23 @@ def start(request):
 class IssueDetailView(generic.DetailView):
     model = Issue
     template_name = 'office/issue.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        issue = Issue.objects.filter(pk=self.kwargs.get('pk'))[0]
+        trustMapping = {TrustTypes.INTERNAL: "internal", TrustTypes.EXTERNAL: "external", TrustTypes.FIELDTEAM: "fieldteam"}
+        context['author_trust_string'] = trustMapping[issue.authorTrust]
+        statusMapping = {StatusTypes.SUBMITTED: "submitted",
+                        StatusTypes.WIP: "wip",
+                        StatusTypes.SOLVED: "solved",
+                        StatusTypes.IMPOSSIBLE: "impossible",
+                        StatusTypes.DUBLICATE: "dublicate"}
+        context['status_string'] = statusMapping[issue.status]
+        positionWGS84 = issue.position
+        positionWGS84.transform(4326)
+        positionWGS84 = positionWGS84.geojson
+        context['position_geojson'] = positionWGS84
+        return context
 
 class IssueListView(SingleTableMixin, FilterView):
     model = Issue
