e470964eb0a81063f5074a5b461294c81a3811ad
matthias <mame@plattform32.de>
2020-02-09 13:24:29 +0100

    fix(test): Changed pinned IDs

--- citysdk/tests.py
+++ citysdk/tests.py
@@ -14,6 +14,8 @@ This includes:
 - URL schema
 - JSON response payload
 - HTTP filter params
+
+see https://github.com/bfpi/klarschiff-citysdk
 """
 
 class CitySDKRequestsTests(TestCase):
@@ -39,7 +41,7 @@ class CitySDKRequestsTests(TestCase):
             assigned=group
         ).save()
         Issue(
-            description='A wip issue',
+            description='A very fresh issue',
             position=Point(54.1, 12.1, srid=4326),
             category=cat,
             created_at=timezone.now(),
@@ -54,12 +56,30 @@ class CitySDKRequestsTests(TestCase):
             created_at=timezone.now(),
             status=StatusTypes.SOLVED,
             published=False,
-            assigned=group
+            assigned=group,
+            photo='testphoto.jpg'
         ).save()
     
-    def test_default_listing(self):
-        """Service requests should look like"""
+    def test_default_listing_filter(self):
+        """Do we get only active requests?"""
         client = APIClient()
-        response = client.get('/citysdk/requests.json', format='json')
+        response = client.get('/citysdk/requests.json?extensions=true')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data), 2)
+    
+    def test_default_listing_content(self):
+        """Do we get only active requests?"""
+        client = APIClient()
+        response = client.get('/citysdk/requests.json?extensions=true')
+        self.assertEqual(response.status_code, 200)
+        print(response.data)
+        activeRequests = [
+            {"service_request_id":2,"extended_attributes":{"detailed_status":"IN_PROCESS","detailed_status_datetime":"2020-01-30T21:52:55.000+01:00","description_public":True,"media_urls":[],"photo_required":True,"trust":1,"votes":0},"status_notes":"Ihre Meldung wurde zur Beräumung des Sperrmülls an die Stadtentsorgung Rostock weitergeleitet!","status":"open","service_code":"29","service_name":"Sperrmüll","description":"Gegenüber der Bleicherstr.31 befindet sich ein Sperrmüllhaufen!","agency_responsible":"Amt für Umweltschutz (delegiert an Stadtentsorgung Rostock GmbH)","service_notice":None,"requested_datetime":"2020-01-30T21:52:47.000+01:00","updated_datetime":"2020-01-30T21:53:31.000+01:00","expected_datetime":"2020-01-31T00:00:00.000+01:00","address":"Bleicherstr. 31 (gegenüber)","adress_id":None,"lat":"54.084745606995064","long":"12.145760492171316","media_url":None,"zipcode":None},
+            {"service_request_id":51578,"extended_attributes":{"detailed_status":"RECEIVED","detailed_status_datetime":"2020-01-30T17:07:46.000+01:00","description_public":False,"media_urls":["http://www.klarschiff-hro.de/citysdk/assets/fotoGross-ed651ba3192856892dfee19b3a099cefdfa35298decd5f6253905403d8045f9a.jpg","http://www.klarschiff-hro.de/citysdk/assets/fotoNormal-8953f1d3359b95db912f315c0b0f6d10e4b4b92ad45d332f85a8829b6f74330e.jpg","http://www.klarschiff-hro.de/citysdk/assets/fotoThumb-55401594568590516a7459e1308150acf164e47e30aebf757871aa76d735d07c.jpg"],"photo_required":False,"trust":0,"votes":0},"status_notes":None,"status":"open","service_code":"5","service_name":"bauliche Gefahrenstelle","description":"redaktionelle Prüfung ausstehend","agency_responsible":"Amt für Verkehrsanlagen","service_notice":None,"requested_datetime":"2020-01-30T17:07:03.000+01:00","updated_datetime":"2020-01-30T17:07:46.000+01:00","expected_datetime":None,"address":"Max-Reichpietsch-Str. 8","adress_id":None,"lat":"54.19408178460182","long":"12.151084000915997","media_url":"http://www.klarschiff-hro.de/citysdk/assets/fotoNormal-8953f1d3359b95db912f315c0b0f6d10e4b4b92ad45d332f85a8829b6f74330e.jpg","zipcode":None}
+        ]
+        #self.assertEqual(response.data, activeRequests)
+
+    # TODO: issue.json
+    
+    # TODO: service.json
+
--- common/tests/tests_issue.py
+++ common/tests/tests_issue.py
@@ -8,6 +8,9 @@ from common.models import Issue, Category
 @patch("requests.post")
 class IssueModelTests(TestCase):
     def test_new_Issue(self, requests_post, get_landowner):
+        """
+        Can we create specific issues?
+        """
         #requests_post.status_code.return_value = 200
         requests_post.json.return_value = {'features': []}
         get_landowner.return_value = 'TEST landowner'
@@ -15,7 +18,7 @@ class IssueModelTests(TestCase):
         cat.save()
         issue = Issue(description="test issue", position=Point(5, 23), category=cat)
         issue.save()
-        self.assertEqual(issue.id, 1)
+        self.assertEqual(len(Issue.objects.all()), 1)
         issue = Issue(id=666, description="test issue with defined id", position=Point(5, 23), category=cat)
         issue.save()
         self.assertEqual(issue.id, 666)
